918a967e451c7fbad741cc5a869d59786505a097
ruydev <duyhua@beae.com>
2023-09-19 19:04:32 +0700

    update path

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import fs from "fs";
+import * as path from "path";
 
 export default defineConfig({ 
     plugins: [vue()],
